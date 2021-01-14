1046d2cc88a4ad560bc61f5ad59c0fa89b937693
Simon Forster <sforster@academytrainee.com>
2021-01-14 11:19:31 +0000

    made calc & button temp color

--- calculator.js
+++ calculator.js
@@ -4,6 +4,11 @@ const result = document.querySelector("#result");
 const history = document.querySelector("#history");
 const clearButton = document.querySelector("#c");
 
+const buttonAdd = document.querySelector("#add");
+const buttonSub = document.querySelector("#sub");
+const buttonMult = document.querySelector("#mult");
+const buttonDiv = document.querySelector("#div");
+
 const getValues = () => {
     return [Number.parseFloat(num1.value), Number.parseFloat(num2.value)];
 }
@@ -13,21 +18,40 @@ const printResult = (newValue) => {
 }
 
 const addToHistory = (num1, num2, operator, result) => {
-    const addParagraph = document.createElement("p");
+    const historyLine = document.createElement("p");
     const info = document.createTextNode(`${num1} ${operator} ${num2} = ${result}`)
-    addParagraph.appendChild(info);
-    history.appendChild(addParagraph);
+    historyLine.appendChild(info);
+    history.appendChild(historyLine);
 }
 
-c.addEventListener("click", function(){ // event listener
+c.addEventListener("click", function(){ // event listener instead of having onClick
     history.innerHTML = "";
 });
+
+const buttonColor = (operation) => { // marks the last button used in light blue
+    buttonAdd.setAttribute("class", "btn btn-primary");
+    buttonSub.setAttribute("class", "btn btn-primary");
+    buttonMult.setAttribute("class", "btn btn-primary");
+    buttonDiv.setAttribute("class", "btn btn-primary");
+    let btn;
+    if (operation === "+") {
+        btn = buttonAdd;
+    } else if (operation === "-") {
+        btn = buttonSub;
+    } else if (operation === "/") {
+        btn = buttonDiv;
+    } else if (operation === "x") {
+        btn = buttonMult;
+    }
+    btn.setAttribute("class", "btn btn-info")
+}
 
 const add = () => {
     const [num1, num2] = getValues();
     const newResult = num1 + num2;
     printResult(newResult);
     addToHistory(num1, num2, "+", newResult);
+    buttonColor("+");
 }
 
 const subtract = () => {
@@ -35,6 +59,7 @@ const subtract = () => {
     const newResult = num1 - num2;
     printResult(newResult);
     addToHistory(num1, num2, "-", newResult);
+    buttonColor("-");
 }
 
 const divide = () => {
@@ -42,6 +67,7 @@ const divide = () => {
     const newResult = num1 / num2;
     printResult(newResult);
     addToHistory(num1, num2, "/", newResult);
+    buttonColor("/");
 }
 
 const multiply = () => {
@@ -49,8 +75,12 @@ const multiply = () => {
     const newResult = num1 * num2;
     printResult(newResult);
     addToHistory(num1, num2, "x", newResult);
+    buttonColor("x");
 }
 
-// window.onload = () => {
-//     prompt("Tell me how you feel");
-// }
+window.onload = () => {
+    const historyLine = document.createElement("p");
+    const info = document.createTextNode("Welcome to calculator!");
+    historyLine.appendChild(info);
+    history.appendChild(historyLine);
+}
